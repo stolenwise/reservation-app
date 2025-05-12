94f3c95705336d084232cea0882dc2510de6c7b7
stolenwise <lewis.r.stone@gmail.com>
2025-05-12 08:48:15 -0700

    reset the db.js with .env creds because the postgresql was out of date.

--- db.js
+++ db.js
@@ -1,8 +1,17 @@
 /** Database for lunchly */
 
+/** Database connection setup for Lunchly */
+
+require("dotenv").config();
 const pg = require("pg");
 
-const db = new pg.Client("postgresql:///lunchly");
+const db = new pg.Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
 
 db.connect();
 
